dffcbdfbce5d0a847dd5d4794536a1a589f0e7df
Sekou <sekou@boltwebsolutions.com>
2016-11-29 02:05:05 -0500

    fixed cellForRowAt method

--- Petitions/ViewController.swift
+++ Petitions/ViewController.swift
@@ -11,17 +11,6 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [[String: String]]()
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let petition = petitions[indexPath.row]
-        cell.textLabel?.text = petition["title"]
-        cell.detailTextLabel?.text = petition["body"]
-        return cell
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +38,18 @@ class ViewController: UITableViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
