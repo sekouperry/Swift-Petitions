9f23e1553b1fdd5ce01b42772171828dd45ae40a
Sekou <sekou@boltwebsolutions.com>
2016-11-29 02:26:45 -0500

    add new tab and error alert

--- Petitions/ViewController.swift
+++ Petitions/ViewController.swift
@@ -15,7 +15,19 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        
+        func showError() {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -23,9 +35,12 @@ class ViewController: UITableViewController {
                 
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
                     parse(json: json)
+                    return
                 }
             }
         }
+        
+        showError()
     }
     
     func parse(json: JSON) {
